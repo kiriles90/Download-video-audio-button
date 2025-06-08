d59f88e69388e776a96e56afde7a7ae099cc4819
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2025-06-08 20:53:01 +0300

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Download video-audio button
 // @namespace   github.com/kiriles90
-// @version     2.3
-// @date        2023-06-09
+// @version     3.0
+// @date        2023-06-10
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Download-video-audio-button/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Download-video-audio-button/master/violentmonkey.js
@@ -13,40 +13,62 @@
 var AKoiMain = {
     vid: null,
     oldUrl: null,
+    observer: null,
     DocOnLoad: function (o) {
         try {
-            if (null != o && null != o.body && null != o.location && ((AKoiMain.vid = AKoiMain.getVid(o)), AKoiMain.vid)) {
-                var d = o.getElementsByClassName('ytd-download-button-renderer')[0].parentNode,
-                    e = o.querySelector("#y2mateconverter"),
-                    n = AKoiMain.GetCommandButton();
-                d.setAttribute("style", "display: inline-block");
-                null == e, (AKoiMain.oldUrl = o.location.href), AKoiMain.checkChangeVid();
+            if (o && o.body && o.location && (AKoiMain.vid = AKoiMain.getVid(o))) {
+                var d = o.getElementsByClassName('ytd-download-button-renderer')[0]?.parentNode;
+                if (!d) return;
+                if (!o.querySelector("#y2mateconverter")) {
+                    var n = AKoiMain.GetCommandButton();
+                    d.style.display = "inline-block";
+                }
+                AKoiMain.oldUrl = o.location.href;
+                AKoiMain.watchUrlChange();
             }
-            return !0;
-        } catch (o) {}
+        } catch (e) {}
     },
-    checkChangeVid: function () {
-        setTimeout(function () { AKoiMain.oldUrl == window.location.href ? AKoiMain.checkChangeVid() : AKoiMain.WaitLoadDom(window.document); }, 1e3);
+    watchUrlChange: function () {
+        if (AKoiMain.observer) AKoiMain.observer.disconnect();
+        AKoiMain.observer = new MutationObserver(() => {
+            if (AKoiMain.oldUrl !== window.location.href) {
+                AKoiMain.oldUrl = window.location.href;
+                AKoiMain.WaitLoadDom(window.document);
+            }
+        });
+        AKoiMain.observer.observe(document.body, { childList: true, subtree: true });
     },
     WaitLoadDom: function (o) {
-        (AKoiMain.vid = AKoiMain.getVid(o)), AKoiMain.vid ? null != o.querySelector("#actions-inner") ? AKoiMain.DocOnLoad(o) : setTimeout(function () { AKoiMain.WaitLoadDom(o); }, 1e3) : AKoiMain.checkChangeVid();
+        AKoiMain.vid = AKoiMain.getVid(o);
+        if (!AKoiMain.vid) {
+            setTimeout(() => AKoiMain.WaitLoadDom(o), 500);
+            return;
+        }
+        if (o.querySelector("#actions-inner")) {
+            AKoiMain.DocOnLoad(o);
+        } else {
+            setTimeout(() => AKoiMain.WaitLoadDom(o), 500);
+        }
     },
-    goToY2mate: function (o) {
-        o.stopPropagation();
+    goToY2mate: function (e) {
+        e.stopPropagation();
         try {
-            var t = "https://y2mate.com/youtube-mp3/" + AKoiMain.vid + "/?utm_source=chrome_addon";
-            window.open(t, "_blank");
-        } catch (o) {}
+            var url = `https://y2mate.com/youtube-mp3/${AKoiMain.vid}/?utm_source=chrome_addon`;
+            window.open(url, "_blank");
+        } catch (e) {}
     },
     GetCommandButton: function () {
         try {
             var o = document.getElementsByTagName("ytd-download-button-renderer")[0];
-            return ((o.id = "y2mateconverter"), o.setAttribute("style", "display: inline-block"), o.addEventListener("click", function (o) { AKoiMain.goToY2mate(o); }, !0), o);
-        } catch (o) {}
+            o.id = "y2mateconverter";
+            o.style.display = "inline-block";
+            o.addEventListener("click", AKoiMain.goToY2mate, true);
+            return o;
+        } catch (e) {}
     },
     getVid: function (o) {
         var t = o.location.toString().match(/^.*((m\.)?youtu\.be\/|vi?\/|u\/\w\/|embed\/|\?vi?=|\&vi?=)([^#\&\?]*).*/);
-        return !(!t || !t[3]) && t[3];
+        return t && t[3];
     },
 };
 AKoiMain.WaitLoadDom(window.document);
