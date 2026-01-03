daa2a8547645ceae934d4513ec5ec51097939ede
kiriles90 <38388670+kiriles90@users.noreply.github.com>
2026-01-03 16:56:59 +0200

    Update violentmonkey.js

--- violentmonkey.js
+++ violentmonkey.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Download video-audio button
 // @namespace   github.com/kiriles90
-// @version     4.6
-// @date        2026-01-03
+// @version     4.8
+// @date        2026-01-04
 // @author      github.com/kiriles90
 // @updateURL   https://raw.githubusercontent.com/kiriles90/Download-video-audio-button/master/violentmonkey.js
 // @downloadURL https://raw.githubusercontent.com/kiriles90/Download-video-audio-button/master/violentmonkey.js
